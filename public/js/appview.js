2b6c8c239cfe437b6cf33e9f8239c4a78ffa5536
JOENCO <161168866+VISITANTE1204@users.noreply.github.com>
2025-06-02 13:37:57 -0500

    Delete public directory

--- public/js/appview.js
+++
@@ -1,6 +0,0 @@
-const botonImagen = document.getElementById('boton-imagen');
-const panelMensaje = document.getElementById('panel-mensaje');
-
-botonImagen.addEventListener('click', () => {
-    panelMensaje.style.display = panelMensaje.style.display === 'block' ? 'none' : 'block';
-});
