dc9f943d833d5dcea1a366689cb713cbee437a12
JOENCO <161168866+VISITANTE1204@users.noreply.github.com>
2025-06-17 08:31:22 -0500

    Update ruotes.js

--- src/ruotes.js
+++ src/ruotes.js
@@ -6,49 +6,38 @@ const incidenciaController = require('./controllers/incidencia.controller');
 const appviewController = require('./controllers/appview.controller');
 const administradorController = require('./controllers/administrador.controller');
 
-//const allowedDomains = ['munipiura.gob.pe'];
-
-/*Código de restricción de dominio
-router.use((req, res, next) => {
-    const domain = req.headers.host;
-    if (allowedDomains.includes(domain)) {
-        next();
-    } else {
-        res.status(403).send('Acceso denegado');
-    }
-});
-*/
-
-// Ruta para la página inicial
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
-// Rutas de autenticación
-router.get('/signup', (req, res) => {
+router.get('/signup.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/signup.html'));
 });
+
+router.get('/appview.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/appview.html'));
+});
+
+router.get('/administrator.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/administrator.html'));
+});
+
+router.get('/helper.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/helper.html'));
+});
+
+router.get('/form.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/form.html'));
+});
+
 router.post('/login', autenticacionController.login);
 router.post('/register', autenticacionController.register);
 router.get('/logout', autenticacionController.logout);
 
-// Rutas de appview
-router.get('/appview', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/appview.html'));
-});
+router.get('/api/incidencias', appviewController.obtenerIncidencias);
+router.post('/api/incidencias', appviewController.crearIncidencia);
 
-router.get('/helper', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/helper.html'));
-});
-
-// Rutas de administrador
-router.get('/administrador', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/administrador.html'));
-});
-
-// Rutas adicionales
-router.get('/form', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/form.html'));
-});
+router.post('/api/incidencias/:id/asignar', administradorController.asignarIncidencia);
+router.put('/api/incidencias/:id/estado', administradorController.actualizarEstadoIncidencia);
 
 module.exports = router;
