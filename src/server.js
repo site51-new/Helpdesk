ac40248e686d34fff2e6596388a2cbf1bf634f79
JOENCO <161168866+VISITANTE1204@users.noreply.github.com>
2025-06-18 18:47:01 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,14 +6,15 @@ const routes = require('./routes');
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.set('views', './docs');  // Cambiado de './views' a './docs'
+app.set('views', './docs');  // Carpeta donde están los archivos HTML
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir archivos estáticos desde docs/public
+// Servir archivos estáticos desde docs/public bajo la ruta /public
 app.use('/public', express.static(path.join(__dirname, '../docs/public')));
 
+// Rutas definidas en routes.js
 app.use('/', routes);
 
 const port = 3000;
