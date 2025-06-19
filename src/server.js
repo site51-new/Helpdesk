aedfde640d2cc75a71f31f00daa67bed3a4f21ba
JOENCO <161168866+VISITANTE1204@users.noreply.github.com>
2025-06-18 22:50:13 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const routes = require('./routes');
 
-app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.set('views', './docs');  // Carpeta donde están los archivos HTML
+app.set('views', path.join(__dirname, '../docs'));
 
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir archivos estáticos desde docs/public bajo la ruta /public
+// Archivos estáticos
 app.use('/public', express.static(path.join(__dirname, '../docs/public')));
 
-// Rutas definidas en routes.js
+// Rutas
 app.use('/', routes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Servidor iniciado en el puerto ${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
