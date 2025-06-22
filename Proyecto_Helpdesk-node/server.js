352d73d34c4a10c67ef04bf3602c5c959bf3da54
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-06-22 16:52:50 -0500

    Arreglar manejo de preflight OPTIONS para CORS

--- Proyecto_Helpdesk-node/server.js
+++ Proyecto_Helpdesk-node/server.js
@@ -13,14 +13,25 @@ const corsOptions = {
     credentials: true,
 };
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', corsOptions.origin);
+    res.header('Access-Control-Allow-Methods', corsOptions.methods.join(','));
+    res.header('Access-Control-Allow-Headers', corsOptions.allowedHeaders.join(','));
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+    next();
+});
+
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../public')));
-
+app.options('*', cors(corsOptions));
 app.use(router);
 
 app.use((err, req, res, next) => {
@@ -29,6 +40,6 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor iniciado en puerto ${port}`);
-});
+app.listen(port, () =>
+    console.log(`Servidor iniciado en puerto ${port}`)
+);
