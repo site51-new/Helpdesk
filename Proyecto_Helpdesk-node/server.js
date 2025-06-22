9885d708720ecc4b59c5889a28c5ab702a77c669
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-06-22 16:03:13 -0500

    Actualiza frontend para CORS y fetch

--- Proyecto_Helpdesk-node/server.js
+++ Proyecto_Helpdesk-node/server.js
@@ -14,8 +14,8 @@ const corsOptions = {
     credentials: true,
 };
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));            
+app.options('*', cors(corsOptions));   
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
