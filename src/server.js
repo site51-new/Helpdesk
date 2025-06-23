e71c13a826d0c8272ed1336dc40fbe06f7b79919
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-06-23 12:18:57 -0500

    Modificacion en server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-.options('/api/incidencias', cors());
+app.options('/api/incidencias', cors());
 
 app.use(express.json());
 app.use(express.static('public'));
