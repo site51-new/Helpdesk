bd54487bff8229bcf9161ba94d2cd27c3d30e58f
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-06-23 09:45:40 -0500

    Modificación en routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,6 @@
-const express = require('express');
+﻿const express = require('express');
 const router = express.Router();
-const path = require('path');
 
-const autenticacionController = require('./controllers/autenticacion.controller');
-const incidenciaController = require('./controllers/incidencia.controller');
-const appviewController = require('./controllers/appview.controller');
-const administradorController = require('./controllers/administrador.controller');
-
-router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')));
-router.get('/signup.html', (req, res) => res.sendFile(path.join(__dirname, '../docs/signup.html')));
-router.get('/appview.html', (req, res) => res.sendFile(path.join(__dirname, '../docs/appview.html')));
-router.get('/administrator.html', (req, res) => res.sendFile(path.join(__dirname, '../docs/administrator.html')));
-router.get('/helper.html', (req, res) => res.sendFile(path.join(__dirname, '../docs/helper.html')));
-router.get('/form.html', (req, res) => res.sendFile(path.join(__dirname, '../docs/form.html')));
-
-router.post('/register', autenticacionController.register);
-router.post('/login', autenticacionController.login);
-router.get('/logout', autenticacionController.logout);
-
-router.get('/api/incidencias', appviewController.obtenerIncidencias);
-router.post('/api/incidencias', appviewController.crearIncidencia);
-
-router.post('/api/incidencias/:id/asignar', administradorController.asignarIncidencia);
-router.put('/api/incidencias/:id/estado', administradorController.actualizarEstadoIncidencia);
+router.get('/', (req, res) => res.send('🚀 Backend activo'));
 
 module.exports = router;
