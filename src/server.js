8796e80a0b563fb091af2b4b7a2375b26fa5da42
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-06-22 23:32:10 -0500

    Modificación de server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use(/.*/, router);
+app.use('/', router);
 
 app.use((req, res, next) => {
     res.status(404).json({ mensaje: 'Ruta no encontrada' });
