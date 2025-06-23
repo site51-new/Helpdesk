3a3e03a6746944e1a9855f4d2c2dcebb78ec00c0
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-06-23 09:26:05 -0500

    Modificación en routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 const autenticacionController = require('./controllers/autenticacion.controller');
-const incidenciaController = require('./controllers/incidencia.controller');
 const appviewController = require('./controllers/appview.controller');
-const administradorController = require('./controllers/administrador.controller');
 
 router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')));
 router.get('/signup.html', (req, res) => res.sendFile(path.join(__dirname, '../docs/signup.html')));
@@ -21,7 +19,12 @@ router.get('/logout', autenticacionController.logout);
 router.get('/api/incidencias', appviewController.obtenerIncidencias);
 router.post('/api/incidencias', appviewController.crearIncidencia);
 
-router.post('/api/incidencias/:id/asignar', administradorController.asignarIncidencia);
-router.put('/api/incidencias/:id/estado', administradorController.actualizarEstadoIncidencia);
+console.log('--- Rutas cargadas (temporal) ---');
+router.stack.forEach(layer => {
+    if (layer.route) {
+        console.log(Object.keys(layer.route.methods).join(',').toUpperCase(), layer.route.path);
+    }
+});
+console.log('--- Fin rutas ---');
 
 module.exports = router;
