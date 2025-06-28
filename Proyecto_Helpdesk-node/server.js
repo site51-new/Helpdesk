a463c1b07d965296eb6dc0f03af3be94d8a2f492
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-06-28 10:41:44 -0500

    Modificación en backend

--- Proyecto_Helpdesk-node/server.js
+++ Proyecto_Helpdesk-node/server.js
@@ -1,4 +1,4 @@
-﻿require('dotenv').config();  
+﻿require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
@@ -6,20 +6,34 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+
+const corsOptions = {
+    origin: 'https://visitante1204.github.io', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
+
 
 app.use(express.json());
 
+
 app.use('/api', routes);
+
 
 app.get('/', (req, res) => {
     res.send('✅ Backend funcionando correctamente');
 });
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Servidor escuchando en puerto ${PORT}`));
 
+
 app.use((err, req, res, next) => {
-    console.error('Error capturado en middleware:', err);
+    console.error('❌ Error capturado en middleware:', err);
     res.status(500).json({ mensaje: 'Error interno del servidor' });
 });
