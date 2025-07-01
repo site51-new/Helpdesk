c49538995866fd567c5ae831c88cda5e186df394
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-07-01 11:17:28 -0500

    Modificación

--- Proyecto_Helpdesk-node/db.js
+++ Proyecto_Helpdesk-node/db.js
@@ -1,15 +1,14 @@
 ﻿const { Pool } = require('pg');
 require('dotenv').config();
 
-if (!process.env.DATABASE_URL) {
-    console.error('❌ DATABASE_URL no está definida');
-    process.exit(1);
-}
-
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT) || 5432,
     ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false  
     }
 });
 
