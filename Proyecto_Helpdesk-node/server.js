5c52f2421429b0fc221570b9146c226afb2749fc
José Enrique Cobeñas <enrriquecov.12@gmail.com>
2025-07-01 08:53:17 -0500

    Modificación de backend

--- Proyecto_Helpdesk-node/server.js
+++ Proyecto_Helpdesk-node/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const pool = require('./db');  
 
 const app = express();
 
@@ -15,12 +16,30 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.options('*', cors(corsOptions)); 
+app.options('*', cors(corsOptions));
 
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
     res.send('✅ Backend funcionando correctamente');
+});
+
+app.get('/test-db', async (req, res) => {
+    try {
+        const query = `SELECT * FROM helpdesk_system."tIncidencias" LIMIT 1;`;
+        const { rows } = await pool.query(query);
+
+        res.json({
+            message: 'Conexión a base de datos OK y tabla accesible',
+            datos: rows.length > 0 ? rows : 'Tabla vacía o sin datos',
+        });
+    } catch (err) {
+        console.error('Error conexión DB o tabla:', err);
+        res.status(500).json({
+            message: 'Error conexión a base de datos o acceso a tabla',
+            error: err.message,
+        });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
